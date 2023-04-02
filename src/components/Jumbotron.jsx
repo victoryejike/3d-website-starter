e96b8b2c5e82571e70c4fa741b5a116489c9a455
Victory Ejike <victory@zestpayment.com>
2023-04-02 22:02:16 +0100

    created the display and webgiviewer sections, also added gsap for animations

--- src/components/Jumbotron.jsx
+++ src/components/Jumbotron.jsx
@@ -3,6 +3,14 @@ import Iphone from "../assets/images/iphone-14.jpg";
 import HoldingIphone from "../assets/images/iphone-hand.png";
 
 const Jumbotron = () => {
+	const handleLearnMore = () => {
+		const element = document.querySelector(".sound-section");
+		window.scrollTo({
+			top: element?.getBoundingClientRect().top,
+			left: 0,
+			behavior: "smooth",
+		});
+	};
 	return (
 		<div className="jumbotron-section wrapper">
 			<h2 className="title">New</h2>
@@ -16,7 +24,7 @@ const Jumbotron = () => {
 					<button className="button">Buy</button>
 				</li>
 				<li>
-					<a href="#" className="link">
+					<a href="#" className="link" onClick={handleLearnMore}>
 						Learn more
 					</a>
 				</li>
--- src/components/SoundSection.jsx
+++ src/components/SoundSection.jsx
@@ -1,7 +1,42 @@
 import React from "react";
 
 const SoundSection = () => {
-	return <div></div>;
+	const handleLearnMore = () => {
+		const element = document.querySelector(".display-section");
+		window.scrollTo({
+			top: element?.getBoundingClientRect().top,
+			left: 0,
+			behavior: "smooth",
+		});
+	};
+	return (
+		<div className="sound-section wrapper">
+			<div className="body">
+				<div className="sound-section-content content">
+					<h2 className="title">New Sound System</h2>
+					<p className="text">Feel the base.</p>
+					<span className="description">
+						From $41.62/mo or $999 before trade-in
+					</span>
+					<ul className="links">
+						<li>
+							<button className="button">Buy</button>
+						</li>
+						<li>
+							<a href="#" className="link">
+								Learn more
+							</a>
+						</li>
+					</ul>
+					{/* <img
+						src={HoldingIphone}
+						alt="Iphone"
+						className="iphone-img"
+					/> */}
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default SoundSection;
